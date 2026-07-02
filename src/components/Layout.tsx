dece57effa62690cada7699dd01d3552e944aaf5
dexSamir <samirah-bp215@code.edu.az>
2026-07-02 12:24:30 +0400

    Always render footer on home

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,15 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
 export function Layout() {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
   return (
     <div className="app-shell">
       <Navbar />
       <main>
         <Outlet />
       </main>
-      {!isHome ? <Footer /> : null}
+      <Footer />
     </div>
   );
 }
